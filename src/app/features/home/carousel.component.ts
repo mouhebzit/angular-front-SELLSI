fb4b5c3b071e5055a07fa70be83a9026229f8e42
Smail Leklou <crespy@MacBook-Pro-de-Smail.local>
2023-10-03 09:21:00 +0200

    Second commit

--- src/app/features/home/carousel.component.ts
+++ src/app/features/home/carousel.component.ts
@@ -22,15 +22,22 @@ export class CarouselComponent {
 
   currentSlide = 0;
   slideWidth = 1500; // This should match the width defined in the CSS
+  disabled_right = "false";
+  disabled_left = "true";
+
 
   previousSlide() {
     this.currentSlide = Math.max(this.currentSlide - 1, 0);
+    this.currentSlide > 0 ? this.disabled_left = "false": this.disabled_left = "true";
+    this.currentSlide === (this.slides.length / 4) - 1 ? this.disabled_right = "true": this.disabled_right = "false";
     console.log("left")
   }
 
   nextSlide() {
     // Adjust for the fact that 4 slides are shown at once.
     this.currentSlide = Math.min(this.currentSlide + 1, Math.ceil(this.slides.length / 4) - 1);
+    this.currentSlide === (this.slides.length / 4) - 1 ? this.disabled_right = "true": this.disabled_right = "false";
+    this.currentSlide > 0 ? this.disabled_left = "false": this.disabled_left = "true";
     console.log("right")
   }
 
--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -5,18 +5,6 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
-  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
-  ngOnInit(): void {
-    this.slides[0] = {
-      src: '../../../assets/slide1.jpg',
-    };
-    this.slides[1] = {
-      src: '../../../assets/slide2.jpg',
-    }
-    this.slides[2] = {
-      src: '../../../assets/slide3.jpg',
-    }
-  }
+export class HomeComponent{
 
 }
